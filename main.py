40e304b33aa112ae0d72097c32c0ae9503871c13
miaobuao <miaobuao@outlook.com>
2025-06-12 15:20:14 +0800

    feat: load recorder configuration from config file

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+from pathlib import Path
+
 from mitm_client.addons import RecorderAddon, ServerSideReplayAddon
 from mitm_client.app.logging_config import setup_logging
 from mitm_client.app.proxy_runner import MitmProxyRunner
 from mitm_client.app.ui import create_ui
+from mitm_client.config import load_config
 from mitm_client.i18n import i18n
 
 
 def main():
+    app_config = load_config(Path("config.json"))
     log_handler = setup_logging()
-    recorder = RecorderAddon()
+    recorder = RecorderAddon(app_config.recorder)
     replay = ServerSideReplayAddon()
     proxy_runner = MitmProxyRunner(recorder, replay)
     proxy_runner.start()
